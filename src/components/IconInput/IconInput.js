8b3148c8fa0ea3bbf15c852e29ba3e3eed993ef7
Nina Wan <ninawan90@gmail.com>
2022-08-14 17:17:19 +1000

    add error message

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,7 +6,7 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const STYLE = {
+const STYLES = {
   small: {
     fontSize: 14,
     iconSize: 16,
@@ -18,9 +18,8 @@ const STYLE = {
     iconSize: 24,
     borderThickness: 2,
     height: 36,
-  }
-
-}
+  },
+};
 
 const IconInput = ({
   label,
@@ -29,7 +28,11 @@ const IconInput = ({
   size,
   ...delegated
 }) => {
-  const styles = STYLE[size];
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`unknown size passed to ProgressBar: ${size}`);
+  }
 
   return (
     <Wrapper>
@@ -37,14 +40,16 @@ const IconInput = ({
       <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
         <Icon id={icon} size={styles.iconSize} />
       </IconWrapper>
-      <TextInput {...delegated} style={{
-        '--width': width + 'px',
-        '--height': styles.height + 'rem',
-        '--border-thickness': styles.borderThickness + 'px',
-        '--font-size': styles.fontSize + 'rem'
-      }} />
+      <TextInput
+        {...delegated}
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem',
+        }}
+      />
     </Wrapper>
-
   );
 };
 
@@ -52,11 +57,10 @@ const Wrapper = styled.label`
   display: block;
   position: relative;
   color: ${COLORS.gray700};
-
   &:hover {
-    color: ${COLORS.black}
+    color: ${COLORS.black};
   }
-`
+`;
 
 const IconWrapper = styled.div`
   position: absolute;
@@ -64,23 +68,22 @@ const IconWrapper = styled.div`
   bottom: 0;
   margin: auto 0;
   height: var(--size);
-`
+`;
 
 const TextInput = styled.input`
   width: var(--width);
   height: var(--height);
+  font-size: var(--font-size);
   border: none;
   border-bottom: var(--border-thickness) solid ${COLORS.black};
   padding-left: var(--height);
   color: inherit;
   font-weight: 700;
-  font-size: var(--font-size);
   outline-offset: 2px;
-
   &::placeholder {
-    color: ${COLORS.gray500};
     font-weight: 400;
+    color: ${COLORS.gray500};
   }
-`
+`;
 
 export default IconInput;
